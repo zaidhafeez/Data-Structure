d233ede5b1bef5b3522550ad2b23c9425ee94614
Zaid <zaid.kpj@gmail.com>
2021-07-11 03:05:36 +0530

    jst

--- javascript/js_learning/first.js
+++ javascript/js_learning/first.js
@@ -1,2 +1,10 @@
-const c = "zaid"
-console.log("Hello World" + " " + c)
+const c = "zaid" //constant
+console.log(typeof c); // know the type of data
+let num = 4 // create the variable 
+console.log(num);
+
+num = 1091910102939040403n // where n is donated the BigInt
+console.log(num);
+
+let bool = 5 < 6
+console.log(bool);
