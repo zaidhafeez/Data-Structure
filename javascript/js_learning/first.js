ed9a9bdd5de523c57e360dc127822ef9570bfb30
Zaid <zaid.kpj@gmail.com>
2021-07-11 15:44:06 +0530

    arithmetic operator

--- javascript/js_learning/first.js
+++ javascript/js_learning/first.js
@@ -7,4 +7,26 @@ num = 1091910102939040403n // where n is donated the BigInt
 console.log(num);
 
 let bool = 5 < 6
-console.log(bool);
+console.log(bool)
+
+num = Number("123") //Explicit conversion
+console.log(num, typeof num);
+
+let x
+console.log(x, typeof x);
+x = 8
+console.log(x, typeof x);
+
+x = x + ""
+console.log(x, typeof x); // type coercion //implicit conversion //string
+// x = x -2
+// console.log(x, typeof x); // type coercion //implicit conversion //number
+
+// x = !x
+// console.log(x, typeof x); // type coercion //implicit conversion //boolean 
+
+x = +x + 2
+console.log(x, typeof x); // type coercion //implicit conversion //number unary operator
+
+x = parseInt("123 Navin") // convert a string into number but remember first character will not be character
+console.log(x);
---
+++ javascript/js_learning/operator.js
@@ -0,0 +1,17 @@
+// // Arithmetic operator
+// let num1 = 4
+// let num2 = 6
+// let result = num1 % num2
+// console.log(result);
+
+// num1 = true
+// num2 = true
+// result = num1 + num2 // this + operator convert this into number // type coercion
+// console.log(result);
+
+
+
+let num = 4
+// let result = Math.pow(4, 3)
+let result = 4 ** 3 // same as power
+console.log(result);
