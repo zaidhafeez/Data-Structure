65330c664e7a45565204d1c39da839f6c1f75934
zaid <zaid.kpj@gmail.com>
2021-07-16 12:04:08 +0530

    arrow fctn

--- javascript/js_learning/objectt.js
+++ javascript/js_learning/objectt.js
@@ -46,13 +46,64 @@ delete alien.tech // delete keyword which is provided by js its delete the objec
 
 // Function
 
-function greet(user){
+// function greet(user){
 
-    return `Hello ${user}`;
+//     return `Hello ${user}`;
 
-}
+// }
 
-let user = 'Zaid';
+// let user = 'Zaidddf';
 
-let str = greet(user);
-console.log(str);
+// let str = greet(user);
+// console.log(str);
+
+// fctn expression
+// let add = function(num1, num2)
+//         {
+//             return num1 + num2;
+//         }
+
+// let sum = add; // this is treating as aN OBJECT
+// let getRes = add(5, 6);
+// getRes = sum(6,9);
+
+// console.log(getRes);
+
+//HOISTING
+// function greet(u){
+
+//     console.log(user);
+
+//     return `Hello ${user}`;
+
+// }
+
+// let user = 'Zaidddf';
+
+// let str = greet(user);
+// console.log(str);
+// Default variable, Local Variable, Global variable
+// function add(num1, num2, num3 = 1) // num1, num2, and num3 are parameters
+// {
+//     // num3 = 1 default value when we dont pass argument any argument in vwill print the default value
+//     console.log(num1, num2, num3);
+//     return num1 + num2 + num3;
+
+// }
+
+// let resultt  = add(5, 6, 5) // 5,6,5 are the arguments
+// console.log("the result is" + resultt);   
+
+// arrow function 
+// we can pass argument too...
+// let greet = (/*parameters too*/) => {
+//     console.log("hello");
+//     return 1;
+// }
+// console.log(greet());
+
+let add = (num1 , num2) => num1 + num2; // when assigning in the variable we use arrow fctn
+let sum = add;
+let res = add(5, 6);
+console.log(res);
+console.log(sum);
